d54137cf1a9c5614af463e8cb4c31ba62605794d
PickishPiper <e.marchiondo@gmail.com>
2022-03-24 15:14:59 -0700

    Forecast Array JS Build
    Incorporated daily forecast features into JS. Located and incorporated indexso only five days forecast displays, rounded min+max temps.

--- src/app.js
+++ src/app.js
@@ -22,56 +22,48 @@ function formatDate(timestamp) {
 return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp){
+let date = new Date(timestamp * 1000);
+let day = date.getDay();
+let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+
+
+
+return days[day];
+
+}
+
 function displayForecast(response) {
-    console.log(response.data.daily);
+    let forecast = response.data.daily;
+
     let forecastElement = document.querySelector("#forecast");
     
     let forecastHTML = `<div class="row">`;
-    let days =["Thur", "Fri", "Sat", "Sun"];
-days.forEach(function (day) {
-    forecastHTML = forecastHTML + 
+forecast.forEach(function (forecastDay, index) {
+if (index < 5){
+
+    forecastHTML = 
+    forecastHTML + 
     `
     <div class="col-2">
-        <div class="weather-forecast-date">
-    ${day}
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}
     </div>
-    <img src="http://openweathermap.org/img/wn/02d@2x.png" 
+    <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" 
     alt=""
     width= 42px 
     />
     <div class="weather-forecast-temperatures"> 
         <span class="weather-forecast-temperature-max">
-    18° |
+    ${Math.round(forecastDay.temp.max)}° |
     </span>
     <span class="weather-forecast-temperature-min">
-    12°
+    ${Math.round(forecastDay.temp.min)}°
     </span>
     </div>
     </div>
     `;
+}
 })
-
-
-forecastHTML = forecastHTML + 
-`<div class="col-2">
-    <div class="weather-forecast-date">
-Fri
-</div>
-<img src="http://openweathermap.org/img/wn/02d@2x.png" 
-alt=""
-width= 42px 
-/>
-<div class="weather-forecast-temperatures"> 
-    <span class="weather-forecast-temperature-max">
-18° |
-</span>
-<span class="weather-forecast-temperature-min">
-12°
-</span>
-</div>
-</div>
-`;
-
 forecastHTML = forecastHTML + `</div>`;
     forecastElement.innerHTML = forecastHTML;
 }
