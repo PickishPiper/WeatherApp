e01d657b512c41fc73ff41fb9a96825016cca93d
PickishPiper <e.marchiondo@gmail.com>
2022-03-24 14:24:31 -0700

    Built Coordinates Sturcture
    Using original Weather API, console logged to find corresponding coordinates per city-search. Combined API key for coordinates function. Console logged API call to locate data.daily for 7 days weather forecast array.

--- src/app.js
+++ src/app.js
@@ -22,7 +22,8 @@ function formatDate(timestamp) {
 return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+    console.log(response.data.daily);
     let forecastElement = document.querySelector("#forecast");
     
     let forecastHTML = `<div class="row">`;
@@ -48,10 +49,7 @@ days.forEach(function (day) {
     </div>
     </div>
     `;
-
 })
-
-
 
 
 forecastHTML = forecastHTML + 
@@ -78,6 +76,13 @@ forecastHTML = forecastHTML + `</div>`;
     forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates){
+    console.log(coordinates);
+    let apiKey = "fd0fa8374c7c322a3c68c2056f8b61b5";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then(displayForecast);
+}
 
 function displayTemperature(response){
 let temperatureElement = document.querySelector("#temperature");
@@ -102,7 +107,17 @@ iconElement.setAttribute( "src",
     );
     iconElement.setAttribute(
         "alt", response.data.weather[0].description);
+
+    
+getForecast(response.data.coord);
+
 }
+
+
+
+
+
+
 
 function search(city) {
     let apiKey = "fd0fa8374c7c322a3c68c2056f8b61b5";
@@ -151,4 +166,3 @@ celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 
 search("Sacramento");
-displayForecast();
