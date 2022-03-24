54ebddb202d413840b2e32b5522d60e1b76e729a
PickishPiper <e.marchiondo@gmail.com>
2022-03-24 13:51:15 -0700

    Active HTML In JS
    Started generating different days of weather forecast for app by integrating active HTML into the JS. Built beginning framework for displayForecast in JS.

--- src/app.js
+++ src/app.js
@@ -22,6 +22,61 @@ function formatDate(timestamp) {
 return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+    let forecastElement = document.querySelector("#forecast");
+    
+    let forecastHTML = `<div class="row">`;
+    let days =["Thur", "Fri", "Sat", "Sun"];
+days.forEach(function (day) {
+    forecastHTML = forecastHTML + 
+    `
+    <div class="col-2">
+        <div class="weather-forecast-date">
+    ${day}
+    </div>
+    <img src="http://openweathermap.org/img/wn/02d@2x.png" 
+    alt=""
+    width= 42px 
+    />
+    <div class="weather-forecast-temperatures"> 
+        <span class="weather-forecast-temperature-max">
+    18° |
+    </span>
+    <span class="weather-forecast-temperature-min">
+    12°
+    </span>
+    </div>
+    </div>
+    `;
+
+})
+
+
+
+
+forecastHTML = forecastHTML + 
+`<div class="col-2">
+    <div class="weather-forecast-date">
+Fri
+</div>
+<img src="http://openweathermap.org/img/wn/02d@2x.png" 
+alt=""
+width= 42px 
+/>
+<div class="weather-forecast-temperatures"> 
+    <span class="weather-forecast-temperature-max">
+18° |
+</span>
+<span class="weather-forecast-temperature-min">
+12°
+</span>
+</div>
+</div>
+`;
+
+forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+}
 
 
 function displayTemperature(response){
@@ -82,6 +137,7 @@ function displayCelsiusTemperature(event){
 let celsiusTemperature = null;
 
 
+
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
@@ -94,4 +150,5 @@ celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 
 
-search("Philadelphia");
+search("Sacramento");
+displayForecast();
